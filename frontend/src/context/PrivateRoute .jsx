c8bb6eb528761980d50c3ebd25e1cdcd50c1a794
Solijonov Muhammad Ali <solijonovmuhammadali0728@gmail.com>
2024-03-26 09:48:33 +0500

    add frontend

---
+++ frontend/src/context/PrivateRoute .jsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "./AuthProvider";
+
+const PrivateRoute = () => {
+  const user = useAuth();
+  console.log(user);
+  if (!user.token) return <Navigate to="/login" />;
+  return <Outlet />;
+};
+
+export default PrivateRoute;
